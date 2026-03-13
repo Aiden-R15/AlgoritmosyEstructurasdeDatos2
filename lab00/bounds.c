08a6501f07df4d00b9b8e94cfc78906f0cb642c2
Aiden-R15 <144642557+Aiden-R15@users.noreply.github.com>
2026-03-13 17:30:31 -0300

    Add files via upload

--- lab00/bounds.c
+++ lab00/bounds.c
@@ -13,35 +13,49 @@ struct bound_data {
 
 struct bound_data check_bound(int value, int arr[], unsigned int length) {
     struct bound_data res;
-    int i = 0;
     res.is_upperbound = true;
     res.is_lowerbound = true;
     res.exists = false;
-    res.where = 0;
-    while (i<lenght) {
-        res.is_upperbound = res.is_upperbound && (value>=a[i]);
-        res.is_lowerbound = res.is_upperbound && (value<=a[i]);
-        res.exists = re.exists || (value==a[i])
-        if (re.exists) {
-            res.where = i;
-        }
-        i = i+1;
+    res.where = -1;
+    for (unsigned int i=0; i<length ; i++) {
+     res.is_upperbound = res.is_upperbound && arr[i]<=value;
+     res.is_lowerbound = res.is_lowerbound && arr[i]>=value;
+     if (value == arr[i]){
+      res.where = i;
+      res.exists = true;
+     }
     }
-
     return res;
 }
 
-int main(void) {
-    int a[ARRAY_SIZE] = {0, -1, 9, 4};
-    int value=9;
+void pedir_arreglo (int n_max, int a[]) {
+    int i;
+    i=0;
+    while (i!=n_max) {
+        printf ("Ingrese un valor para la posicion %d del arreglo:\n", i);
+        scanf ("%d", &a[i]);
+        i = i+1;
+    }    
+}
 
-    struct bound_data result = check_bound(value, a, ARRAY_SIZE);
+
+int main(void) {
+    //int a[ARRAY_SIZE] = {0, -1, 9, 4};
+    int value;
+    unsigned int size;
+    printf ("Ingrese el tamaño del arreglo: ");
+    scanf ("%u", &size);
+    int a[size];
+    pedir_arreglo (size, a);
+    printf ("Ingrese un valor entero: ");
+    scanf ("%d", &value);
+    struct bound_data result = check_bound(value, a, size);
 
     // TODO: REEMPLAZAR LO SIGUIENTE LUEGO POR LA SALIDA QUE PIDE EL ENUNCIADO
-    printf("%d", result.is_upperbound); // Imprime 1
-    printf("%d", result.is_lowerbound); // Imprime 0
-    printf("%u", result.exists);        // Imprime 1
-    printf("%u", result.where);         // Imprime 2
+    printf("%d\n", result.is_upperbound); // Imprime 1
+    printf("%d\n", result.is_lowerbound); // Imprime 0
+    printf("%u\n", result.exists);        // Imprime 1
+    printf("%u\n", result.where);         // Imprime 2
 
     return EXIT_SUCCESS;
 }
