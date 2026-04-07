c5ec710756ae1d1fe714b65879fa9805b18e8c11
Aiden-R15 <144642557+Aiden-R15@users.noreply.github.com>
2026-04-07 17:23:45 -0300

    Add files via upload

---
+++ lab01/ej1/sort.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static void insert(int a[], unsigned int i, unsigned int length) {
+    while (i>0 && a[i]<a[i-1]) {
+        array_dump (a, length);
+        swap (a,i,(i-1));
+        i = i-1;
+    }
+}
+
+void insertion_sort(int a[], unsigned int length) {
+    for (unsigned int i = 1; i < length; ++i) {
+        insert(a, i, length);
+        array_dump (a, length);
+        assert(!array_is_sorted(a,length));
+    }
+}
